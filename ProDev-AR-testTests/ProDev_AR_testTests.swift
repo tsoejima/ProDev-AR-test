5aa4395d228382f28fbe9438c1b8246f353834f5
tsoejima <t.soejimadeveloper@gmail.com>
2020-09-28 18:38:48 +0900

    Initial Commit

---
+++ ProDev-AR-testTests/ProDev_AR_testTests.swift
@@ -0,0 +1,33 @@
+//
+//  ProDev_AR_testTests.swift
+//  ProDev-AR-testTests
+//
+//  Created by 副島拓哉 on 2020/09/28.
+//
+
+import XCTest
+@testable import ProDev_AR_test
+
+class ProDev_AR_testTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
